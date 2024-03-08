0fb31a6b7f51d8da768091ddc3f07eb4033acdfa
Rohit Goud <krohitgoud@gmail.com>
2024-03-07 21:31:43 -0500

    Update PrimesServiceApplicationTests.java

--- src/test/java/com/iu/rokala/primesservice/PrimesServiceApplicationTests.java
+++ src/test/java/com/iu/rokala/primesservice/PrimesServiceApplicationTests.java
@@ -10,4 +10,30 @@ class PrimesServiceApplicationTests {
 	void contextLoads() {
 	}
 
+	PrimesService primes = new PrimesService();
+	@Test
+	void _45() {
+		int n =45;
+		boolean expected = false;
+		boolean actual = primes.isPrime(n);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void _532893258045L() {
+		long n =532893258045L;
+		boolean expected = false;
+		boolean actual = primes.isPrime(n);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void _285191() {
+		long n =285191;
+		boolean expected = true;
+		boolean actual = primes.isPrime(n);
+		assertEquals(expected, actual);
+	}
+
+
 }
